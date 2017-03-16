07fb588efcbe60ca1537b37b967280ade20bf5fb
Kaldari <rkaldari@wikimedia.org>
2017-03-15 21:28:33 -0700

    adding table schema

---
+++ checklist.sql
@@ -0,0 +1,8 @@
+--
+-- Table structure for table `checklist`
+--
+
+CREATE TABLE IF NOT EXISTS `checklist` (
+  `project` varchar(255) DEFAULT NULL,
+  `updated` datetime DEFAULT NULL
+) CHARSET=utf8;
