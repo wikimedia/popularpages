148e4a92f2a4704101869cf0e1beb45b3ee38756
Niharika Kohli <niharikakohli29@gmail.com>
2017-03-30 11:14:50 -0700

    Do composer install in travis instead of adding vendor

--- vendor/mediawiki/mediawiki-codesniffer/MediaWiki/Tests/files/ExtraCharacters/extra_characters_before_phpopen_tag_pass.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Valid php open tag
-function wfFoo() {
-
-}
-?>
-T<?php // this php open tag will in any case be ignored
--- vendor/mediawiki/mediawiki-codesniffer/utils/gen-changelog.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Generate changelog for HISTORY.md based on git log
-git log --format='* %s (%aN)' --no-merges --reverse $(git describe --abbrev=0)...HEAD | sort
